4a4a1ea24622acc007b957e093f0a823fa3e8a9c
ps0m <ps0m@tut.by>
2022-02-18 01:53:01 +0300

    add: funcion shuffleCard

--- index.js
+++ index.js
@@ -1,24 +1,70 @@
-
 let quantityElements = 16;
+
 let gameField = document.querySelector('.game-field');
-let card = document.createElement ('div');
-let z;
-for (i=1; i<=quantityElements; i++) {
-    let card = document.createElement ('div');
-    card.classList.add ('card');
-    card.setAttribute('data-number', i);
-    card.style.background = `#fef4d1 url(./assets/png/${i}.png)`;
-    card.style.backgroundSize = "cover";
-    gameField.append(card);
+let currentCard = null;
+let firstCard, secondCard;
+// function create class in css
+function addRule(stylename, selector, rule) {
 
-    card.addEventListener("click", (e)=> {
-        z = e.target.dataset;
-        e.target.style.removeProperty("background");
-        e.target.classList.toggle ('card__close');
-        console.log(z)});
+    let stylesheet = document.querySelector(`link[href$="${stylename}.css"]`);
+    stylesheet.sheet.insertRule(selector + ' { ' + rule + ' }', stylesheet.sheet.cssRules.length);
+       }
+  
+// create block card
 
+function newCard () {
+    let arr = shuffleCard();
+    for (i=1; i<=quantityElements; i++) {
+        let card = document.createElement ('div');
+        card.classList.add ('card');
+        // card.classList.add (`card__close`);
+        card.style.backgroundSize = "cover";
+        card.classList.add (`card__open${i}`);
+        
+        card.setAttribute('data-number', arr[i-1]);
+        addRule('style', `.card__open${arr[(i-1)/2]}`, `background: url(./assets/png/${arr[(i-1)/2]}.png)`);
+        
+        gameField.append(card);
+    }
 }
+newCard ();
 
 
+let cardList = document.querySelectorAll('.card');
+
+cardList.forEach(element => {
+    element.addEventListener("click", (e)=> {
+    e.target.classList.toggle (`card__open${e.target.dataset.number}`);
+    e.target.classList.toggle ('card__close');
+    console.log(e);
+        if (!currentCard) {
+            currentCard = true;
+            firstCard = e.target.dataset.number;
+        } else {
+            secondCard = e.target.dataset.number;
+            currentCard = null;
+        } 
+        if (firstCard === currentCard) {
+            console.log('ok');
+        }
+        console.log(currentCard, firstCard, secondCard);
+
+    })    
+});
 
 
+function shuffleCard() {
+    let array = [];
+    for (let i = 1; i <= quantityElements; i++) {
+        array.push(i);
+    }
+    for (let i = 0; i <array.length; i++) {
+        let current = array[i]
+        let rand = Math.floor(Math.random()*array.length);
+        // array[i] = rand;
+        array[i] = array[rand];
+        array[rand] = current;
+    } 
+    return array;
+} 
+// console.log(shuffleCard());

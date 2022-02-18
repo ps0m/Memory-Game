fc11d0e9dc14a6b4038bb4b1d9b2bda9507c2ee6
ps0m <ps0m@tut.by>
2022-02-18 14:35:07 +0300

    feat: add minimal game functional

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ let quantityElements = 16;
 let gameField = document.querySelector('.game-field');
 let currentCard = null;
 let firstCard, secondCard;
+
+let checkTurn = false; 
+let freezeField = false;
 // function create class in css
 function addRule(stylename, selector, rule) {
 
@@ -14,15 +17,15 @@ function addRule(stylename, selector, rule) {
 
 function newCard () {
     let arr = shuffleCard();
-    for (i=1; i<=quantityElements; i++) {
+    for (i=0; i<quantityElements; i++) {
         let card = document.createElement ('div');
         card.classList.add ('card');
-        // card.classList.add (`card__close`);
+        card.classList.add (`card__close`);
         card.style.backgroundSize = "cover";
-        card.classList.add (`card__open${i}`);
-        
-        card.setAttribute('data-number', arr[i-1]);
-        addRule('style', `.card__open${arr[(i-1)/2]}`, `background: url(./assets/png/${arr[(i-1)/2]}.png)`);
+        // card.classList.add (`card__open${i}`);
+        card.setAttribute('data-class', i );
+        card.setAttribute('data-number', Math.ceil(arr[(i)]/2));
+        addRule('style', `.card__open${i}`, `background: url(./assets/png/${Math.ceil(arr[(i)]/2)}.png)`);
         
         gameField.append(card);
     }
@@ -33,38 +36,76 @@ newCard ();
 let cardList = document.querySelectorAll('.card');
 
 cardList.forEach(element => {
-    element.addEventListener("click", (e)=> {
-    e.target.classList.toggle (`card__open${e.target.dataset.number}`);
-    e.target.classList.toggle ('card__close');
-    console.log(e);
-        if (!currentCard) {
-            currentCard = true;
-            firstCard = e.target.dataset.number;
-        } else {
-            secondCard = e.target.dataset.number;
-            currentCard = null;
-        } 
-        if (firstCard === currentCard) {
-            console.log('ok');
-        }
-        console.log(currentCard, firstCard, secondCard);
-
-    })    
+        element.addEventListener("click", turnCard )
 });
 
+function turnCard (){
+    if (freezeField){
+        return;
+    } 
+    
+    if (!currentCard) {
+        this.classList.toggle (`card__open${this.dataset.class}`);
+        firstCard = this; 
+        currentCard = true;
+        console.log(firstCard);
+        checkTurn = firstCard.dataset.class;
+        
+    } else {
+        if (checkTurn === this.dataset.class) {
+            return;
+        } 
+        this.classList.toggle (`card__open${this.dataset.class}`);
+        secondCard = this;
+        currentCard = null;
+        console.log(secondCard);
+        checkMatch (); 
+    } 
+}
+
+function checkMatch () {
+    if ((firstCard.dataset.number === secondCard.dataset.number)) {
+            firstCard.removeEventListener ("click", turnCard);
+            secondCard.removeEventListener ("click", turnCard);
+            newStep ()
+            console.log('!!');
+    } else {
+        unTurnCard ();
+    } 
+} 
+
+function unTurnCard () {
+    freezeField = true;
+    setTimeout (() => {
+    firstCard.classList.remove (`card__open${firstCard.dataset.class}`);
+    secondCard.classList.remove (`card__open${secondCard.dataset.class}`);
+    freezeField = false;
+    newStep ()
+    }, 1000)
+
+}
+
+function newStep () {
+    firstCard = secondCard =null;
+    checkTurn = freezeField = false; 
+} 
 
 function shuffleCard() {
     let array = [];
     for (let i = 1; i <= quantityElements; i++) {
         array.push(i);
     }
-    for (let i = 0; i <array.length; i++) {
-        let current = array[i]
-        let rand = Math.floor(Math.random()*array.length);
-        // array[i] = rand;
-        array[i] = array[rand];
-        array[rand] = current;
-    } 
+    // for (let i = 0; i <array.length; i++) {
+    //     let current = array[i]
+    //     let rand = Math.floor(Math.random()*array.length);
+    //     // array[i] = rand;
+    //     array[i] = array[rand];
+    //     array[rand] = current;
+    // } 
     return array;
 } 
 // console.log(shuffleCard());
+
+
+
+
